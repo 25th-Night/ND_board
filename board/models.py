66e3bbee6375605a22ddade95e0ce5b088dc658c
25th-Night <brown_eyed87@naver.com>
2023-05-31 20:58:57 +0900

    Add Attendance, Question Model

--- board/models.py
+++ board/models.py
@@ -1,8 +1,7 @@
 from typing import Iterable, Optional
 from django.db import models
+from enum import Enum
 
-# Profile Table
-# 이름, 나이, 이메일, 소개 + x, y
 
 
 class BaseModel(models.Model):
@@ -33,3 +32,38 @@ class Profile(BaseModel):
     class Meta:
         verbose_name = "프로필"
         verbose_name_plural = "프로필 목록"
+
+
+class Attendance(BaseModel):
+    ATTENDANCE = 1
+    ABSENT = 2
+    PARTIAL = 3
+    ATTENDANCE_TYPE = [
+        (ATTENDANCE, "출석"),
+        (ABSENT, "결석 (사유 필히 작성)"),
+        (PARTIAL, "일부 일정 불참 (사유 필히 작성)"),
+    ]
+    name = models.CharField(verbose_name="이름", max_length=10)
+    date = models.DateField(verbose_name="날짜")
+    status = models.PositiveSmallIntegerField(verbose_name="출석여부", choices=ATTENDANCE_TYPE, unique=True)
+    reason = models.TextField(verbose_name="사유", null=True, blank=True)
+
+    def __str__(self):
+        return self.get_attendance_type_display()
+
+    class Meta:
+        verbose_name = "카테고리"
+        verbose_name_plural = "카테고리 목록"
+
+
+class Question(BaseModel):
+    title = models.CharField(verbose_name="제목", max_length=30)
+    content = models.TextField(verbose_name="내용")
+    screenshot = models.ImageField(verbose_name="스크린샷", default="/static/img/명탐정피카20.gif")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "질문"
+        verbose_name_plural = "질문 목록"
