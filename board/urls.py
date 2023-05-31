4a49d9b140574c278c356107c35d0d200fdce31a
25th-Night <brown_eyed87@naver.com>
2023-05-31 22:36:10 +0900

    Add url path related Attendance, Question

--- board/urls.py
+++ board/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import *
+from board.views import *
 
 urlpatterns = [
+    path('attendance/', AttendanceListView.as_view(), name="attendance_list"),
+    path('attendance/create/', AttendanceCreateView.as_view(), name="attendance_create"),
+    path('question/', QuestionListView.as_view(), name="question_list"),
+    path('question/<int:id>/', QuestionDetailView.as_view(), name="question_detail"),
+    path('question/create/', QuestionCreateView.as_view(), name="question_create"),
+
 
 ]
