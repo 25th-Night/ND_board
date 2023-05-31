29ff9f02132be405f7d57a0db418f3b2f4b16ecc
25th-Night <brown_eyed87@naver.com>
2023-05-31 22:34:58 +0900

    Add view func related to Attendance, Question

--- board/views.py
+++ board/views.py
@@ -1,52 +1,75 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views import View
 from django.views.generic import TemplateView
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 
-# Function Based View
-# FBV
-def index_function(request, code, name):
-    if request.method == 'GET':
-        context = {
-            "method": request.method,
-            "user": request.user,
-            "greeting": "안녕하세요",
-            "code": code,
-            "name": name,
-        }
-        return render(request, "index.html", context)
-    if request.method == 'POST':
-        return HttpResponse("index by POST FBV function called")
-    
-def index_function2(request):
-    if request.method == 'GET':
-        query_string = request.GET.get('qs')
-        context = {
-            "method": request.method,
-            "user": request.user,
-            "greeting": "안녕하세요",
-            "query_string" : query_string
-        }
-        return render(request, "index.html", context)
-    if request.method == 'POST':
-        return HttpResponse("index by POST FBV function2 called")
+from board.models import Attendance, Question
+
+
+class AttendanceListView(TemplateView):
+    template_name = "attendance/list.html"
+
+    def get_context_data(self, **kwargs):
+
+        context = {}
+
+        attendances = Attendance.objects.all().order_by('-created_at')
+        context['attendances'] = attendances
+
+        return context
     
 
-# Class Based View
-# CBV
-@method_decorator(csrf_exempt, name='dispatch')
-class IndexClass(View):
-    def get(self, request):
-        return HttpResponse("index by GET CBV function called")
+class AttendanceCreateView(TemplateView):
+    template_name = "attendance/create.html"
+
+    def get_context_data(self, **kwargs):
+
+        context = {}
+
+        return context
     
     def post(self, request):
-        return HttpResponse("index by POST CBV function called")
-    
-class IndexClass2(TemplateView):
-    template_name = "index.html"
+        return redirect('attendance_list')
 
-    def get(self, request):
-        response = super(IndexClass2, self).get(self, request)
-        return response
+
+class QuestionListView(TemplateView):
+    template_name = "question/list.html"
+
+    def get_context_data(self, **kwargs):
+
+        context = {}
+
+        questions = Question.objects.all().order_by("-created_at")
+        context['questions'] = questions
+
+        return context
+    
+
+class QuestionDetailView(TemplateView):
+    template_name = "question/create.html"
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+        question_id = kwargs['id']
+        question = get_object_or_404(Question, id=question_id)
+        context['question'] = question
+
+        return context
+    
+
+class QuestionCreateView(TemplateView):
+    template_name = "question/list.html"
+
+    def get_context_data(self, **kwargs):
+
+        context = {}
+
+        return context
+
+        
+    
+    def post(self, request):
+        return redirect('question_list')
