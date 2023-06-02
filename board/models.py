c16451869ee6bf215798f3d414e3408f1affe54a
25th-Night <brown_eyed87@naver.com>
2023-06-02 16:53:43 +0900

    Remove Profile Model

--- board/models.py
+++ board/models.py
@@ -1,4 +1,3 @@
-from typing import Iterable, Optional
 from django.db import models
 from enum import Enum
 
@@ -10,28 +9,6 @@ class BaseModel(models.Model):
 
     class Meta:
         abstract = True
-
-
-class Profile(BaseModel):
-    name = models.CharField(verbose_name="이름", max_length=10)
-    age = models.PositiveSmallIntegerField(verbose_name="나이")
-    email = models.EmailField(verbose_name="이메일", blank=True)
-    description = models.TextField(verbose_name="소개", blank=True)
-
-    def __str__(self):
-        return f"{self.name}의 프로필"
-    
-    def save(self, force_insert: bool = ..., force_update: bool = ..., using: str | None = ..., update_fields: Iterable[str] | None = ...) -> None:
-        # 저장 될 때마다 수행되는 로직
-        # 결제에서 order_code -> 결제 주문서가 생성될 때 난수 값으로 만듦
-        return super().save(force_insert, force_update, using, update_fields)
-    
-    def get_name(self):
-        return f"{self.name}"
-    
-    class Meta:
-        verbose_name = "프로필"
-        verbose_name_plural = "프로필 목록"
 
 
 class Attendance(BaseModel):
@@ -49,11 +26,11 @@ class Attendance(BaseModel):
     reason = models.TextField(verbose_name="사유", null=True, blank=True)
 
     def __str__(self):
-        return self.get_attendance_type_display()
+        return self.get_status_display()
 
     class Meta:
-        verbose_name = "카테고리"
-        verbose_name_plural = "카테고리 목록"
+        verbose_name = "출결"
+        verbose_name_plural = "출결 목록"
 
 
 class Question(BaseModel):
